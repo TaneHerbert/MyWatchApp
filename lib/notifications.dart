c9a5a649f7042607d1de88ba92614748e811cbc1
Tane Herbert <taneherbert@Tanes-MBP.hub>
2023-01-10 18:37:57 +1300

    Started notifications section

--- lib/notifications.dart
+++ lib/notifications.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
-class Notifications extends StatelessWidget {
+class Notifications extends StatefulWidget {
   const Notifications({super.key});
+
+  @override
+  State<Notifications> createState() => _NotificationsState();
+}
+
+class _NotificationsState extends State<Notifications> {
+  bool light = true;
 
   @override
   Widget build(BuildContext context) {
@@ -23,6 +31,182 @@ class Notifications extends StatelessWidget {
             icon: const Icon(Icons.info_outline),
           ),
         ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.only(left: 25, top: 10, right: 25),
+        child: Column(
+          children: [
+            Container(
+              width: double.infinity,
+              decoration: BoxDecoration(
+                color: Colors.orange,
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.only(left: 20, right: 5),
+                child: Column(
+                  children: [
+                    SizedBox(
+                      height: 40,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          const Icon(FontAwesomeIcons.instagram),
+                          const Padding(
+                            padding: EdgeInsets.only(left: 10),
+                            child: Text("Instagram"),
+                          ),
+                          const Spacer(flex: 1),
+                          Switch(
+                            // This bool value toggles the switch.
+                            value: light,
+                            activeColor: Colors.red,
+                            onChanged: (bool value) {
+                              // This is called when the user toggles the switch.
+                              setState(() {
+                                light = value;
+                              });
+                            },
+                          ),
+                        ],
+                      ),
+                    ),
+                    SizedBox(
+                      height: 40,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          const Icon(
+                            FontAwesomeIcons.snapchat,
+                            color: Colors.black,
+                          ),
+                          const Padding(
+                            padding: EdgeInsets.only(left: 10),
+                            child: Text("Snapchat"),
+                          ),
+                          const Spacer(flex: 1),
+                          Switch(
+                            // This bool value toggles the switch.
+                            value: light,
+                            activeColor: Colors.red,
+                            onChanged: (bool value) {
+                              // This is called when the user toggles the switch.
+                              setState(() {
+                                light = value;
+                              });
+                            },
+                          ),
+                        ],
+                      ),
+                    ),
+                    SizedBox(
+                      height: 40,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          const Icon(FontAwesomeIcons.twitter),
+                          const Padding(
+                            padding: EdgeInsets.only(left: 10),
+                            child: Text("Twitter"),
+                          ),
+                          const Spacer(flex: 1),
+                          Switch(
+                            // This bool value toggles the switch.
+                            value: light,
+                            activeColor: Colors.red,
+                            onChanged: (bool value) {
+                              // This is called when the user toggles the switch.
+                              setState(() {
+                                light = value;
+                              });
+                            },
+                          ),
+                        ],
+                      ),
+                    ),
+                    SizedBox(
+                      height: 40,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          const Icon(FontAwesomeIcons.youtube),
+                          const Padding(
+                            padding: EdgeInsets.only(left: 10),
+                            child: Text("Youtube"),
+                          ),
+                          const Spacer(flex: 1),
+                          Switch(
+                            // This bool value toggles the switch.
+                            value: light,
+                            activeColor: Colors.red,
+                            onChanged: (bool value) {
+                              // This is called when the user toggles the switch.
+                              setState(() {
+                                light = value;
+                              });
+                            },
+                          ),
+                        ],
+                      ),
+                    ),
+                    SizedBox(
+                      height: 40,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          const Icon(
+                            Icons.mail,
+                            size: 28,
+                          ),
+                          const Padding(
+                            padding: EdgeInsets.only(left: 10),
+                            child: Text("Mail"),
+                          ),
+                          const Spacer(flex: 1),
+                          Switch(
+                            // This bool value toggles the switch.
+                            value: light,
+                            activeColor: Colors.red,
+                            onChanged: (bool value) {
+                              // This is called when the user toggles the switch.
+                              setState(() {
+                                light = value;
+                              });
+                            },
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            const SizedBox(height: 30),
+            Container(
+              width: double.infinity,
+              decoration: BoxDecoration(
+                color: Colors.red,
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.only(left: 20, right: 20),
+                child: Column(
+                  children: const [
+                    SizedBox(
+                      height: 50,
+                      child: Align(
+                          alignment: Alignment.center,
+                          child: Text(
+                            "Clear All",
+                            style: TextStyle(color: Colors.white),
+                          )),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
