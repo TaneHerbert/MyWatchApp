df30149b58cdccd7fe8a9602cae98cd311d72e5b
Tane Herbert <taneherbert@Tanes-MBP.hub>
2023-01-10 17:48:51 +1300

    General Display and Brightness Page

--- lib/displayAndBrigtness.dart
+++ lib/displayAndBrigtness.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
 
-class DisplayAndBrightness extends StatelessWidget {
+class DisplayAndBrightness extends StatefulWidget {
   const DisplayAndBrightness({super.key});
+
+  @override
+  State<DisplayAndBrightness> createState() => _DisplayAndBrightnessState();
+}
+
+class _DisplayAndBrightnessState extends State<DisplayAndBrightness> {
+  double _currentSliderValue = 0.0;
+  bool light = true;
 
   @override
   Widget build(BuildContext context) {
@@ -23,6 +31,135 @@ class DisplayAndBrightness extends StatelessWidget {
             icon: const Icon(Icons.info_outline),
           ),
         ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.only(left: 25, top: 10, right: 25),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Padding(
+              padding:
+                  EdgeInsets.only(left: 10, right: 10, top: 10, bottom: 10),
+              child: Text(
+                "Brightness",
+                style: TextStyle(
+                  color: Colors.grey,
+                ),
+              ),
+            ),
+            Container(
+              width: double.infinity,
+              decoration: BoxDecoration(
+                color: Colors.orange,
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.only(left: 10, right: 10),
+                child: Column(
+                  children: [
+                    SizedBox(
+                      height: 40,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          const Flexible(
+                            flex: 1,
+                            child: Icon(Icons.cloud, size: 25),
+                          ),
+                          Flexible(
+                            flex: 15,
+                            child: Slider(
+                              value: _currentSliderValue,
+                              max: 100,
+                              divisions: 5,
+                              label: _currentSliderValue.round().toString(),
+                              onChanged: (double value) {
+                                setState(() {
+                                  _currentSliderValue = value;
+                                });
+                              },
+                            ),
+                          ),
+                          const Flexible(
+                            flex: 2,
+                            child: Icon(Icons.sunny, size: 25),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            const SizedBox(height: 30),
+            Container(
+              width: double.infinity,
+              decoration: BoxDecoration(
+                color: Colors.orange,
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.only(left: 20, right: 5),
+                child: Column(
+                  children: [
+                    SizedBox(
+                      height: 40,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          const Text("Always On"),
+                          Switch(
+                            // This bool value toggles the switch.
+                            value: light,
+                            activeColor: Colors.red,
+                            onChanged: (bool value) {
+                              // This is called when the user toggles the switch.
+                              setState(() {
+                                light = value;
+                              });
+                            },
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            const Padding(
+              padding: EdgeInsets.only(left: 10, right: 10, top: 10),
+              child: Text(
+                "Your watch can always show the time. This will cause the battery to decrease very quick.",
+                style: TextStyle(color: Colors.grey),
+              ),
+            ),
+            const SizedBox(height: 30),
+            Container(
+              width: double.infinity,
+              decoration: BoxDecoration(
+                color: Colors.orange,
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.only(left: 20, right: 20),
+                child: Column(
+                  children: [
+                    SizedBox(
+                      height: 40,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: const [
+                          Text("Wake Duraion"),
+                          Text(">"),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
