0d2abc96bc64908e9db844e2b9c3f21289d79523
Tane Herbert <taneherbert@Tanes-MBP.hub>
2023-01-10 17:56:46 +1300

    Sound and volume page started

--- lib/soundAndVolume.dart
+++ lib/soundAndVolume.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
 
-class SoundAndVolume extends StatelessWidget {
+class SoundAndVolume extends StatefulWidget {
   const SoundAndVolume({super.key});
+
+  @override
+  State<SoundAndVolume> createState() => _SoundAndVolumeState();
+}
+
+class _SoundAndVolumeState extends State<SoundAndVolume> {
+  double _currentSliderValue = 0.0;
+  bool light = true;
 
   @override
   Widget build(BuildContext context) {
@@ -23,6 +31,88 @@ class SoundAndVolume extends StatelessWidget {
             icon: const Icon(Icons.info_outline),
           ),
         ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.only(left: 25, top: 10, right: 25),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Padding(
+              padding:
+                  EdgeInsets.only(left: 10, right: 10, top: 10, bottom: 10),
+              child: Text(
+                "Volume",
+                style: TextStyle(
+                  color: Colors.grey,
+                ),
+              ),
+            ),
+            Container(
+              width: double.infinity,
+              decoration: BoxDecoration(
+                color: Colors.orange,
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.only(left: 10, right: 10),
+                child: Column(
+                  children: [
+                    SizedBox(
+                      height: 40,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          const Flexible(
+                            flex: 1,
+                            child: Icon(Icons.volume_down, size: 25),
+                          ),
+                          Flexible(
+                            flex: 15,
+                            child: Slider(
+                              value: _currentSliderValue,
+                              max: 100,
+                              divisions: 5,
+                              label: _currentSliderValue.round().toString(),
+                              onChanged: (double value) {
+                                setState(() {
+                                  _currentSliderValue = value;
+                                });
+                              },
+                            ),
+                          ),
+                          const Flexible(
+                            flex: 2,
+                            child: Icon(Icons.volume_up, size: 25),
+                          ),
+                        ],
+                      ),
+                    ),
+                    SizedBox(
+                      height: 40,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          const Text("Silent Mode"),
+                          Switch(
+                            // This bool value toggles the switch.
+                            value: light,
+                            activeColor: Colors.red,
+                            onChanged: (bool value) {
+                              // This is called when the user toggles the switch.
+                              setState(() {
+                                light = value;
+                              });
+                            },
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
